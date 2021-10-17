c8f0625b4f14722eb9f20e9345d2857a3f1706ff
SvetlanaYes <91842237+SvetlanaYes@users.noreply.github.com>
2021-10-17 15:15:32 +0400

    Updated rm code

--- rm/rm.c
+++ rm/rm.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int main(int argc, char**argv ) {
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+int main(int argc, char*argv[] ) {
  if (argc < 2) {
    printf("Error");
    return 1;
  }
+ int d = strcmp(argv[1],"-f");
 
- 
- for (int i = 1;i<argc;++i){
-  
+if (d == 0) {
+   for (int i = 2;i<argc;++i){
     int result  = remove(argv[i]);
+
+    
     if(result!=0){
-      printf("Error\n");
+
+       
+      printf("Something is wrong!\n");
     }
     else 
      {
-      printf("Ok\n");
+      printf("%d directory is removed \n,",i - 1);
+ 
+   }
+  }
+  return 0;
+}
+ 
+ for (int i = 1;i<argc;++i){
+    DIR* directory = opendir(argv[i]);
+    if(directory!=NULL){
+     closedir(directory);
+     printf(" -f is missing\n");
+     return 0;
+     }
+     
+    int result  = remove(argv[i]);
+    if(result!=0){
+      printf("Cannot remove file %d\n",i);
+    }
+    else 
+     {
+      printf("%d File successfully removed\n",i);
  
    }
   }
  
   return 0;
 }
-
