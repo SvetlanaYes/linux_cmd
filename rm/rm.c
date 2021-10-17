bde535e62702991002098af272a3e4242b32157f
jivan <jivan@ispras.ru>
2021-10-17 17:15:24 +0400

    Rewrite rm.c old version

--- rm/rm.c
+++ rm/rm.c
@@ -1,55 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-
-int main(int argc, char*argv[] ) {
+int main(int argc, char**argv ) {
  if (argc < 2) {
    printf("Error");
    return 1;
  }
- int d = strcmp(argv[1],"-f");
 
-if (d == 0) {
-   for (int i = 2;i<argc;++i){
-    int result  = remove(argv[i]);
-
-    
-    if(result!=0){
-
-       
-      printf("Something is wrong!\n");
-    }
-    else 
-     {
-      printf("%d directory is removed \n,",i - 1);
- 
-   }
-  }
-  return 0;
-}
  
  for (int i = 1;i<argc;++i){
-    DIR* directory = opendir(argv[i]);
-    if(directory!=NULL){
-     closedir(directory);
-     printf(" -f is missing\n");
-     return 0;
-     }
-     
+  
     int result  = remove(argv[i]);
     if(result!=0){
-      printf("Cannot remove file %d\n",i);
+      printf("Error\n");
     }
     else 
      {
-      printf("%d File successfully removed\n",i);
+      printf("Ok\n");
  
    }
   }
  
   return 0;
 }
+
