07a90718cfbce68fe626a6a24e16091a3827fc3d
SvetlanaYes <yesayan.svetlana@student.rau.am>
2021-11-27 02:01:35 +0400

    Grep-code-recursively-added

---
+++ cd/rm/CMakeFiles/rm.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/svetlana/linux_cmd/rm/rm.c" "rm/CMakeFiles/rm.dir/rm.c.o" "gcc" "rm/CMakeFiles/rm.dir/rm.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
